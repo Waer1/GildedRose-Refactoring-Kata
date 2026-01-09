a84cf960b12b0b1f77da57efcddc7a5942b8d9c3
waer <yousef.elwaer@rabbitmart.com>
2026-01-09 19:08:59 +0200

    test: add characterization test for normal item quality decrease

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,12 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  describe('Normal item', () => {
+    it('decreases quality and sellIn by 1 each day', () => {
+      const gildedRose = new GildedRose([new Item('Normal Item', 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(19);
+    });
   });
 });
