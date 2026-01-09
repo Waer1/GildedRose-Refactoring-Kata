55971aef1199a2e4e70c2fff8b159f598aa68b1b
waer <yousef.elwaer@rabbitmart.com>
2026-01-09 21:13:11 +0200

    use increaseQuality function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -38,14 +38,10 @@ export class GildedRose {
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == this.BACKSTAGE_PASSES) {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < this.MAX_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+              this.items[i].quality = this.increaseQuality(this.items[i].quality)
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < this.MAX_QUALITY) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+              this.items[i].quality = this.increaseQuality(this.items[i].quality)
             }
           }
         }
@@ -69,9 +65,7 @@ export class GildedRose {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
         } else {
-          if (this.items[i].quality < this.MAX_QUALITY) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+          this.items[i].quality = this.increaseQuality(this.items[i].quality)
         }
       }
 
