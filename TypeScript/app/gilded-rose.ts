b9ce50244418fddc5f1b33cf773652e7a9b3a08f
waer <yousef.elwaer@rabbitmart.com>
2026-01-09 19:26:38 +0200

    introduce AGED_BRIE constant

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -11,6 +11,8 @@ export class Item {
 }
 
 export class GildedRose {
+  private readonly AGED_BRIE = 'Aged Brie';
+
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
@@ -19,13 +21,17 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
+
       } else {
+
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -41,10 +47,12 @@ export class GildedRose {
             }
           }
         }
+
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -62,6 +70,9 @@ export class GildedRose {
           }
         }
       }
+
+
+
     }
 
     return this.items;
